3d0995a63c4a1996e8a9518225f2b75b38bfe845
Kr4v3n <fafachen37@gmail.com>
2020-01-14 09:07:59 +0100

    Adding movies search

--- src/movie/urls.py
+++ src/movie/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import MovieDetail, MovieList, MovieCategory, MovieLanguage
+from .views import MovieDetail, MovieList, MovieCategory, MovieLanguage, MovieSearch
 
 app_name = 'movie'
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', MovieList.as_view(), name='movie_list'),
     path('category/<str:category>', MovieCategory.as_view(), name='movie_category'),
     path('language/<str:lang>', MovieLanguage.as_view(), name='movie_language'),
+    path('search/', MovieSearch.as_view(), name='movie_search'),
     path('<int:pk>', MovieDetail.as_view(), name='movie_detail'),
 ]
--- src/movie/views.py
+++ src/movie/views.py
@@ -18,11 +18,13 @@ class MovieDetail(DetailView):
         object = super(MovieDetail, self).get_object()
         object.views_count += 1
         object.save()
+
         return object
 
     def get_context_data(self, **kwargs):
         context = super(MovieDetail, self).get_context_data(**kwargs)
         context['links'] = Movielinks.objects.filter(movie=self.get_object())
+
         return context
 
 
@@ -32,11 +34,13 @@ class MovieCategory(ListView):
 
     def get_queryset(self):
         self.category = self.kwargs['category']
+
         return Movie.objects.filter(category=self.category)
 
     def get_context_data(self, **kwargs):
         context = super(MovieCategory, self).get_context_data(**kwargs)
         context['movie_category'] = self.category
+
         return context
 
 
@@ -51,4 +55,21 @@ class MovieLanguage(ListView):
     def get_context_data(self, **kwargs):
         context = super(MovieLanguage, self).get_context_data(**kwargs)
         context['movie_language'] = self.language
+
         return context
+
+
+class MovieSearch(ListView):
+    model = Movie
+    paginate_by = 2
+
+    def get_queryset(self):
+        query = self.request.GET.get('query')
+        if query:
+            object_list = self.model.objects.filter(title__icontains=query)
+            print(query)
+            print(object_list)
+        else:
+            object_list = self.model.objects.none()
+
+        return object_list
